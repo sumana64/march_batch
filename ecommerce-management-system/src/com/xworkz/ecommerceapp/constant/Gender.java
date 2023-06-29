fa7f5f0df6b128e514b5b73e17590ed3ab9c8f84
sumana64 <sumanahr.xworkz@gmail.com>
2023-06-29 10:13:11 +0530

    commiting java files

--- ecommerce-management-system/src/com/xworkz/ecommerceapp/constant/Gender.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.ecommerceapp.constant;
-
-public enum Gender {
-
-        male,female,transgender;
-
-
-
-}
---
+++ hospital1-management-system/src/com/xworkz/hospitalapp/patient/constant/Address.java
@@ -0,0 +1,14 @@
+package com.xworkz.hospitalapp.patient.constant;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Address {
+
+  private String address;
+
+  Country country;
+
+}
